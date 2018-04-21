4d4c68f1e0a9880772537d7886dd1e196f34edcc
SusaHitomi <a17dc092@dhw.ac.jp>
2018-04-21 19:39:12 +0900

    jyankenschool

---
+++ app/src/main/java/com/kayosystem/honki/chapter02/jyankenschool/MainActivity.java
@@ -0,0 +1,45 @@
+package com.kayosystem.honki.chapter02.jyankenschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView player;
+    TextView cpu;
+    TextView result;
+    int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        player=(TextView)findViewById(R.id.player);
+        cpu=(TextView)findViewById(R.id.cpu);
+        result=(TextView)findViewById(R.id.result);
+    }
+
+    public void goo(View v) {
+        player.setText("あなたの手はグーです");
+        Random random = new Random();
+        number = random.nextInt(3);
+
+        if (number == 0) {
+            cpu.setText("相手の手はグーです");
+            result.setText("引き分けです");
+
+        } else if (number == 1) {
+            cpu.setText("相手の手はチョキです");
+            result.setText("あなたの勝ちです");
+
+        } else if (number == 2) {
+            cpu.setText("相手の手はパーです");
+            result.setText("あなたの負けです");
+        }
+    }
+}
